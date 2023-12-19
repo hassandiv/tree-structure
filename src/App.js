7cf62a446509b584d3bbcec23291275d733df8e2
Hassan Hafez <laptop@laptops-MacBook.local>
2023-12-19 18:27:59 +0200

    added files array, created entry component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+
+const files = {
+	children: [
+		{
+			name: "node_modules",
+			children: [
+				{
+					name: "joi",
+					children: [
+						{
+							name: "node_modules",
+						},
+						{
+							name: "package.json",
+						},
+					],
+				},
+			],
+		},
+		{
+			name: "package.json",
+		},
+		{
+			name: "vite.config.ts",
+		},
+	],
+};
+
+function Entry({ name, children }) {
+	return (
+		<div>
+			{name}
+			{children?.map((entry) => (
+				<Entry {...entry} />
+			))}
+		</div>
+	);
+}
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className="App">
+			{files.children.map((entry) => (
+				<Entry {...entry} />
+			))}
+		</div>
+	);
 }
 
 export default App;
